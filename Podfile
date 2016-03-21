83a551fd1e2c8f526e5b90a528c6eeb8c8d4851a
riku arakawa <a_ra_ra_rikky0611@yahoo.co.jp>
2016-03-21 20:21:25 +0900

    initial commit

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '8.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+target 'test_Firebase' do
+pod 'Firebase', '>= 2.4.3'
+end
+
+target 'test_FirebaseTests' do
+
+end
+
+target 'test_FirebaseUITests' do
+
+end
+
--- test_Firebase/ViewController.swift
+++ test_Firebase/ViewController.swift
@@ -7,14 +7,38 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    var ref = Firebase(url:"https://hem-tutorial-0318.firebaseio.com")
+    @IBOutlet var label : UILabel!
+    var num = 0
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        ref.queryLimitedToLast(25).observeEventType(FEventType.ChildAdded, withBlock: { (snapshot) in
+            print(snapshot.value!)
+            self.num = snapshot.value["num"] as! Int
+            self.label.text = String(self.num)
+        })
+        
+
+        
     }
 
+    @IBAction func plus(){
+        num++
+        let post = ["num":num]
+        let post1Ref = ref.childByAutoId()
+        post1Ref.setValue(post)
+        label.text = String(num)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
